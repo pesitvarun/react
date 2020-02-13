f544652e452215a50473ade2108b196c365fe878
Varun Sharma <VarunS@interrait.info>
2020-02-14 01:28:33 +0530

    Updates

---
+++ precache-manifest.577e682fa65ec8992f30bd9c0ae32441.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d76e3279f83942619ab1daea15bf57d",
+    "url": "/reactrouter/index.html"
+  },
+  {
+    "revision": "a28d8e093a0123c29daa",
+    "url": "/reactrouter/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "dd185ecff4b53cd59fea",
+    "url": "/reactrouter/static/css/main.6fa208b6.chunk.css"
+  },
+  {
+    "revision": "a28d8e093a0123c29daa",
+    "url": "/reactrouter/static/js/2.c4292a4c.chunk.js"
+  },
+  {
+    "revision": "379303f1c15bc822883b2b350ac25c44",
+    "url": "/reactrouter/static/js/2.c4292a4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd185ecff4b53cd59fea",
+    "url": "/reactrouter/static/js/main.0a98e13f.chunk.js"
+  },
+  {
+    "revision": "180afb2a083024758d30",
+    "url": "/reactrouter/static/js/runtime-main.eecdb0e6.js"
+  },
+  {
+    "revision": "a2d262d4dcb4c257754fac3ff170dacf",
+    "url": "/reactrouter/static/media/homeHandiCraft.a2d262d4.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactrouter/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react/precache-manifest.828d687acda7223c45677c432ca7b5d0.js"
+  "/reactrouter/precache-manifest.577e682fa65ec8992f30bd9c0ae32441.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactrouter/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
