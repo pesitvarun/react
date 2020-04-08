45000a32f17170f3617ab20d68939f0988a3a0fa
varun <pesitvarun@gmail.com>
2020-04-08 22:41:48 +0530

    Updates

---
+++ precache-manifest.bdeca8561c2e1ab3715a387d6426db03.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a13f1869d5c486019925",
+    "url": "/react/static/css/main.28207211.chunk.css"
+  },
+  {
+    "revision": "a13f1869d5c486019925",
+    "url": "/react/static/js/main.adc991d1.chunk.js"
+  },
+  {
+    "revision": "4e6a4b677d1c1e6ba359",
+    "url": "/react/static/js/runtime~main.bb50a20d.js"
+  },
+  {
+    "revision": "783a9242b21e783ee95d",
+    "url": "/react/static/css/2.83c6892b.chunk.css"
+  },
+  {
+    "revision": "783a9242b21e783ee95d",
+    "url": "/react/static/js/2.40deea75.chunk.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "a2d262d4dcb4c257754fac3ff170dacf",
+    "url": "/react/static/media/homeHandiCraft.a2d262d4.jpg"
+  },
+  {
+    "revision": "6d572bfeb1757327a7e99156e60f6d54",
+    "url": "/react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react/precache-manifest.68c3ce6ca4eb8b6b75d990d87739f8a0.js"
+  "/react/precache-manifest.bdeca8561c2e1ab3715a387d6426db03.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react/index.html"), {
+workbox.routing.registerNavigationRoute("/react/index.html", {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
